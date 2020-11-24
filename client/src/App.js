d2f3c06ecf921f07ebc457cd7d9c6801da0f5b58
Shafin5714 <shafin5714@gmail.com>
2020-11-24 14:02:56 +0600

    User authentication forntend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router , Route} from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
@@ -13,9 +16,13 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-           <Route path="/" component={HomeScreen} exact/>
+           
+           <Route path="/login" component={LoginScreen}  exact/>
+           <Route path="/register" component={RegisterScreen}  exact/>
+           <Route path="/profile" component={ProfileScreen}  exact/>
            <Route path="/product/:id" component={ProductScreen}/>
            <Route path="/cart/:id?" component={CartScreen}/> 
+           <Route path="/" component={HomeScreen} exact/>
         </Container>
       </main>
       <Footer/>
